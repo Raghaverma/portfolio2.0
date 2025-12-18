ed998a44d6df9157f37d67949656bc40319515b3
Raghav Verma <rvraghav09@gmail.com>
2025-12-18 15:59:58 +0530

    Remove logo/favicon from browser tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,23 +29,6 @@ export const metadata: Metadata = {
     "Software Engineer",
   ],
   authors: [{ name: "Raghav Verma" }],
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
 }
 
 export default function RootLayout({
